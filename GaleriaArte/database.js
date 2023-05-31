f7a726edd910ea49c44d291fc2b55663fd2febf9
Mazku <82379670+Idiame@users.noreply.github.com>
2023-05-31 13:15:32 +0000

    favicon añadido

--- GaleriaArte/database.js
+++ GaleriaArte/database.js
@@ -7,17 +7,17 @@ const pool = mysql.createPool(database)
 console.log('conexion con la base de datos correctamente')
 
 
-module.exports = {
-    query: (...args) => {
-      return new Promise((resolve, reject) => {
-        pool.query(...args)
-          .then(([result]) => resolve(result))
-          .catch(reject)
-          .finally(() => {
-            pool.end(); // Cierra el pool de conexiones
-          });
-      });
-    },
-  };
+// module.exports = {
+//     query: (...args) => {
+//       return new Promise((resolve, reject) => {
+//         pool.query(...args)
+//           .then(([result]) => resolve(result))
+//           .catch(reject)
+//           .finally(() => {
+//             pool.end(); // Cierra el pool de conexiones
+//           });
+//       });
+//     },
+//   };
 
-// module.exports = pool
+module.exports = pool
